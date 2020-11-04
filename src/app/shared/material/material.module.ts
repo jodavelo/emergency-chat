d558ce9d321efc542e77bf7c6486df3c157bde9c
Daniel Vergara Lozano <josedani0630@hotmail.com>
2020-11-04 02:26:28 -0500

    Initial commit

---
+++ src/app/shared/material/material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+
+const modules = [
+  MatIconModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ...modules
+  ],
+  exports: [
+    ...modules
+  ]
+})
+export class MaterialModule { }
